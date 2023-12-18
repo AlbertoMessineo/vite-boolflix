d924a48c6ac8a05532b51c8eb702b8a8d59bd13e
AlbertoMessineo <albevorox@gmail.com>
2023-12-18 23:48:29 +0100

    feat: searchbar + API calls

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
 <script>
 import { store } from './store';
+import axios from 'axios';
 export default {
-  name: 'boolflix',
-
-  data() {
-    return {
-      text: 'Template VueJS',
-    };
+  name: 'Boolflix',
+  data(){
+    return{
+      store,
+      errorMessage:'',
+    }
   },
+  methods: {
+    search(){
+     axios.get(store.config.apiMoviesUrl, {
+      params:{
+        api_key: store.config.apiKey,
+        query: store.searchKey,
+      }
+     })
+     .then((response)=> {
+      console.log(response);
+     })
+     .catch((error) => {
+      this.errorMessage = error.message;
+     });
+    },
+  },
+
 };
 </script>
 
@@ -15,7 +33,9 @@ export default {
   <form @submit.prevent="search">
     <label for="search"> Search </label>
     <input type="text" id="search" v-model="store.searchKey">
+    <button> Search </button>
   </form>
+  <div v-if="errorMessage">{{ errorMessage }}</div>
 </template>
 
 <style scoped lang="scss">
